894da7a7d729b7f3474302a35aba843a60ea44e7
LaamGinghong <33472924+LaamGinghong@users.noreply.github.com>
2021-03-11 14:18:19 +0800

    refactor: infrastructure (#4)
    modify eslint for typescript
    
    fix generate tool's bug

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,76 @@
 module.exports = {
   env: {
     browser: true,
-    es2020: true,
+    es2021: true,
+    node: true,
   },
-  extends: ['airbnb-base', 'prettier'],
+  extends: [
+    'airbnb-base',
+    'plugin:eslint-comments/recommended',
+    'plugin:import/typescript',
+    'plugin:unicorn/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint', 'prettier'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.js', '.json'],
+      },
+    },
+  },
   rules: {
     'prettier/prettier': 'error',
+    'unicorn/filename-case': ['error', { cases: { camelCase: true, kebabCase: true } }],
+    'unicorn/prevent-abbreviations': [
+      'error',
+      {
+        replacements: {
+          params: {
+            parameters: false,
+          },
+          dir: {
+            directory: false,
+            direction: false,
+          },
+        },
+      },
+    ],
+    'import/extensions': [
+      2,
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        json: 'never',
+        js: 'never',
+      },
+    ],
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': 'error',
   },
+  overrides: [
+    {
+      files: ['**/*.d.ts'],
+      rules: {
+        'import/no-duplicates': 'off',
+      },
+    },
+    {
+      files: ['scripts/**/*.ts'],
+      rules: {
+        'no-console': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'unicorn/no-process-exit': 'off',
+        'unicorn/import-style': 'off',
+        'unicorn/prevent-abbreviations': 'off',
+      },
+    },
+  ],
 }
